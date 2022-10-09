73def2c9a3f096c96cd4aeb09f25a2fd448d3bcb
dhk767 <dhk767@gmail.com>
2022-10-09 17:39:35 +0900

    Initial Commit

---
+++ TTWorldClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTWorldClass
+//
+//  Created by 김동현 on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
