ae837d593d5c5a58c034342cd659b19e6f838ab2
dhk767 <dhk767@gmail.com>
2022-10-09 18:43:04 +0900

    TT 프로젝트 초기 화면설정(launch screen, tutorial screen 화면 구성)

--- TTWorldClass/ViewController.swift
+++ TTWorldClass/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stepsImages: UIImageView!
+    var number: Int = 1
+    @IBOutlet weak var dots: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func swipeAction(_ sender: Any) {
+        number += 1
+        if number == 4{
+            stepsImages.isHidden = true
+            dots.isHidden = true
+        } else{
+            stepsImages.image = UIImage(named: "step\(String(number))")
+            dots.currentPage = number - 1
+        }
+    }
+    
 }
 
