85872e95bf7d6737c4a23d674f7275a5ba21f9c3
dhk767 <dhk767@gmail.com>
2022-10-10 18:36:55 +0900

    feature : 타이머 기능 구현, start 버튼 구현 중

--- TTWorldClass/TimerViewController.swift
+++ TTWorldClass/TimerViewController.swift
@@ -8,14 +8,58 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var lblTime: UILabel!
+    
+    var timer = Timer()
+    var isTimerStarted = false
+    var time = 1500
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func btnPlayTapped(_ sender: Any) {
+        if !isTimerStarted{
+            startTimer()
+            isTimerStarted = true
+            
+        }else{
+            timer.invalidate()
+            isTimerStarted = false
+            
+        }
+               
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        time -= 1
+        lblTime.text = formatTime()
+    }
+    
+    func formatTime() -> String{
+        let hour = Int(time / 3600)
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hour, minutes, seconds)
+        
+    }
 
+    @IBAction func btnReset(_ sender: Any) {
+        timer.invalidate()
+        time = 1500
+        isTimerStarted = false
+        lblTime.text = "00:25:00"
+    }
+
+    
     /*
     // MARK: - Navigation
 
