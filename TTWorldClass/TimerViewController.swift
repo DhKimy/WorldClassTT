27cfb89cc0b3bbe0ecc8e8aefb2eab98e637bfa1
dhk767 <dhk767@gmail.com>
2022-10-16 17:20:18 +0900

    feature : 현재 할 일 알람과 전체 시간 2개 알람만 두는 버전으로 수정. 기능 구현 중

--- TTWorldClass/TimerViewController.swift
+++ TTWorldClass/TimerViewController.swift
@@ -1,17 +1,15 @@
 import UIKit
+import AVFoundation
 
 class TimerViewController: UIViewController {
     
     @IBOutlet weak var stackTimerLabel: UILabel!
-    
-    
     @IBOutlet weak var entireTimerLabel: UILabel!
-    
     @IBOutlet weak var currentTimerLabel: UILabel!
     
     @IBOutlet weak var timeButton: UIButton!
     
-    
+    /*
     // 실제로 지나간 시간 표시에 필요한 필드
     var stackTimer: Timer? = nil
     var isTimerOnForStackSecond = false
@@ -27,6 +25,7 @@ class TimerViewController: UIViewController {
             stackTimerLabel.text = "\(hours):\(minutes):\(seconds)"
         }
     }
+     */
     
     // 전체 남은 시간 표시에 필요한 빌드
     var entireTimer: Timer? = nil
@@ -67,54 +66,156 @@ class TimerViewController: UIViewController {
         notificationCenter.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     
+//    // 전체 남은 시간용 appMovedToBackground() 메서드
+//    @objc func appMovedToBackground() {
+//        print("App moved to background!")
+//        if isTimerOnForStackSecond {
+//            timeWhenGoBackgroundForStackSecond = Date()
+//            print("Save")
+//        }
+//    }
+    
+//    // 전체 남은 시간용 appMoveToForeground() 메서드
+//    @objc func appMovedToForeground() {
+//        print("App moved to foreground")
+//        if let backTime = timeWhenGoBackgroundForStackSecond {
+//            let elapsed = Date().timeIntervalSince(backTime)
+//            let duration = Int(elapsed)
+//            timeStackSecond += duration
+//            timeEntireSecond -= duration
+//            timeCurrentSecond -= duration
+//            timeWhenGoBackgroundForStackSecond = nil
+//            print("DURATION: \(duration)")
+//        }
+//    }
+    
     
     @objc func appMovedToBackground() {
         print("App moved to background!")
-        if isTimerOnForStackSecond {
-            timeWhenGoBackgroundForStackSecond = Date()
+        if isTimerOnForEntire {
+            timeWhenGoBackgroundForEntire = Date()
             print("Save")
         }
     }
     
     @objc func appMovedToForeground() {
         print("App moved to foreground")
-        if let backTime = timeWhenGoBackgroundForStackSecond {
+        if let backTime = timeWhenGoBackgroundForEntire {
             let elapsed = Date().timeIntervalSince(backTime)
             let duration = Int(elapsed)
-            timeStackSecond += duration
-            timeEntireSecond -= duration
-            timeCurrentSecond -= duration
-            timeWhenGoBackgroundForStackSecond = nil
+            if timeEntireSecond - duration <= 0 {
+                timeEntireSecond = 0
+                timeCurrentSecond = 0
+                timeWhenGoBackgroundForEntire = nil
+                isTimerOnForEntire = !isTimerOnForEntire
+            }else {
+                timeEntireSecond -= duration
+                timeCurrentSecond -= duration
+                timeWhenGoBackgroundForEntire = nil
+                isTimerOnForEntire = !isTimerOnForEntire
+            }
             print("DURATION: \(duration)")
         }
     }
     
+
+    
     @IBAction func timeBtnClicked(_ sender: Any) {
-        if isTimerOnForStackSecond {
-            stackTimer?.invalidate()
+        
+        if isTimerOnForEntire {
             currentTimer?.invalidate()
             entireTimer?.invalidate()
             timeButton.setTitle("START", for: .normal)
+            
         } else {
-            self.stackTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { stackTimer in
-                self.timeStackSecond += 1
-                print("\(self.timeStackSecond)")
-                
-            }
-            RunLoop.current.add(self.stackTimer!, forMode: .common)
+            
+//            // 전체 흐른 시간용 타이머
+//            self.stackTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { stackTimer in
+//                self.timeStackSecond += 1
+//                print("\(self.timeStackSecond)")
+//
+//            }
+//            RunLoop.current.add(self.stackTimer!, forMode: .common)
+            
             self.entireTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {entireTimer in
                 self.timeEntireSecond -= 1
+                if self.timeEntireSecond == 0 {
+                    self.finalEndingAlert()
+                    self.timeCurrentSecond -= 1
+                    self.currentTimer?.invalidate()
+                    self.entireTimer?.invalidate()
+                    self.timeButton.setTitle("Start", for: .normal)
+                    self.isTimerOnForEntire = !self.isTimerOnForEntire
+                    
+                }
                 print("\(self.timeEntireSecond)")
             }
+            
             RunLoop.current.add(self.entireTimer!, forMode: .common)
+            
+            
             self.currentTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {currentTimer in
                 self.timeCurrentSecond -= 1
+                if self.timeCurrentSecond == 0 && self.timeEntireSecond != 0
+                {
+                    self.endingAlert()
+                    self.timeEntireSecond -= 1
+                    self.currentTimer?.invalidate()
+                    self.entireTimer?.invalidate()
+                    self.timeButton.setTitle("New Start", for: .normal)
+                    self.isTimerOnForEntire = !self.isTimerOnForEntire
+                }
+                
                 print("\(self.timeCurrentSecond)")
             }
             RunLoop.current.add(self.currentTimer!, forMode: .common)
             
             timeButton.setTitle("STOP", for: .normal)
+            
         }
-        isTimerOnForStackSecond = !isTimerOnForStackSecond
+            
+       
+        isTimerOnForCurrent = !isTimerOnForCurrent
+        isTimerOnForEntire = !isTimerOnForEntire
+    }
+    
+    
+    var player:AVAudioPlayer!
+    // 시간 도과 시 울리는 알람에 필요한 메서드
+    func playSound(title: String?){
+            let url = Bundle.main.url(forResource: "Balynt - Memory", withExtension: "mp3")
+            player = try!AVAudioPlayer(contentsOf: url!)
+            player!.play()
+        }
+         
+    func stopSound(){
+        player?.stop()
+    }
+
+    func endingAlert(){
+        self.playSound(title: "Alarm_sound")
+        let alert = UIAlertController(title : "완료!", message:"축하합니다. 이번 할 일을 모두 끝냈습니다.", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "알람 소리 끄기", style: .default) {
+            (action) in
+            self.stopSound()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: false, completion: nil)
+        
+        return
+    }
+
+    func finalEndingAlert() {
+        self.playSound(title: "Alarm_sound")
+        let alert = UIAlertController(title : "완료!", message:"축하합니다. 모든 할 일을 모두 끝냈습니다.", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "알람 소리 끄기", style: .default) {
+            (action) in
+            self.stopSound()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: false, completion: nil)
+        
+        return
+        
     }
 }
